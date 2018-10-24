d3130984ee6076e17ee2ed30178f83fdbc0f98cb
Samsoedien <samsoedien@wlan-202220.nbw.tue.nl>
2018-10-24 19:47:30 +0200

    optimised transform file

--- transform.js
+++ transform.js
@@ -12,30 +12,14 @@ const output = []; //puts output in empty array
 const userIds = {}; //puts output in object notation
 let nextId = 1;
 
+//const agendaItems = [];
+let agendaItems;
+
 for (const inItem of sleepInput) {
-  let agendaItems;
-  for (const inItem of calendarInput) {
-    agendaItems = inItem;
-    //console.log(agendaItems);
-  }
-  // for (const inItem of calendarInput) {
-  //   // const outItem = {};
-
-  //   // outItem.activity = inItem.activity;
-  //   // outItem.period = inItem.period;
-
-  //   // outItem.agendaData = inItem;
-
-  //   // output.push(outItem);
-
-  //   let agendaItems = inItem.start;
-  //   return agendaItems;
-  // }
-
   const outItem = {};
 
   // copy _id field
-  outItem._id = inItem._id;
+  outItem._id = inItem._id.$oid;
 
   // map userID
   const inId = inItem.userId;
@@ -45,9 +29,6 @@ for (const inItem of sleepInput) {
     userIds[inId] = outId;
   }
   outItem.userId = outId;
-
-  console.log(agendaItems);
-  outItem.agendaData = agendaItems;
 
   // date of the night
   // outItem.unconvertedSessionStartTime = inItem.sessionStartTimeIsoDate.$date;
@@ -84,13 +65,42 @@ for (const inItem of sleepInput) {
   }
 
   // calculations on sleepstages
-  timeSession = outItem.count_SLEEP_STAGES * 30;
+  let timeSession = outItem.count_SLEEP_STAGES * 30;
   //outItem.timeSleep = outItem.timeSession - outItem.count_WAKE * 30;
 
   outItem.timeSessionMinutes = (outItem.count_SLEEP_STAGES * 30) / 60;
   outItem.timeSleepMinutes = (timeSession - outItem.count_WAKE * 30) / 60;
 
   // put into file
+  output.push(outItem);
+}
+
+for (const inItem of calendarInput) {
+  const outItem = {};
+
+  agendaItems = inItem;
+
+  if (agendaItems.colorId === "10") {
+    agendaItems.colorId = "Travel";
+  } else if (agendaItems.colorId === "11") {
+    agendaItems.colorId = "Work";
+  } else if (agendaItems.colorId === "3") {
+    agendaItems.colorId = "Leisure";
+  } else if (agendaItems.colorId === "5") {
+    agendaItems.colorId = "Sport";
+  } else if (agendaItems.colorId === "6") {
+    agendaItems.colorId = "Children";
+  } else if (agendaItems.colorId === "9") {
+    agendaItems.colorId = "Chores";
+  } else {
+    //agendaItems.colorId = "Activity not defined";
+  }
+
+  outItem.eventSummary = agendaItems.summary;
+  outItem.eventActivity = agendaItems.colorId;
+  outItem.eventStart = agendaItems.start.dateTime;
+  outItem.eventEnd = agendaItems.end.dateTime;
+
   output.push(outItem);
 }
 
